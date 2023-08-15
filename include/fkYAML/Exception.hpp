71fb843e20440b1939bff0966c0d67cd9275abdb
fktn <fktn.dev@gmail.com>
2023-08-15 22:57:12 +0900

    added doxygen-style comments to fkYAML library source files

--- include/fkYAML/Exception.hpp
+++ include/fkYAML/Exception.hpp
@@ -1,5 +1,6 @@
 /**
- * Exception.hpp - implementation of custom exception classes.
+ * @file Exception.hpp
+ * @brief Implementation of custom exception classes.
  *
  * Copyright (c) 2023 fktn
  * Distributed under the MIT License (https://opensource.org/licenses/MIT)
@@ -11,26 +12,48 @@
 #include <stdexcept>
 #include <string>
 
+/**
+ * @namespace fkyaml
+ * @brief namespace for fkYAML library.
+ */
 namespace fkyaml
 {
 
+/**
+ * @class Exception
+ * @brief A base exception class used in fkYAML library.
+ */
 class Exception : public std::exception
 {
 public:
+    /**
+     * @brief Construct a new Exception object without any error messages.
+     */
     Exception() = default;
 
+    /**
+     * @brief Construct a new Exception object with an error message.
+     *
+     * @param msg An error description message.
+     */
     explicit Exception(const char* msg)
         : m_error_msg(msg)
     {
     }
 
 public:
+    /**
+     * @brief Returns an error message internally held. If nothing, a non-null, empty string will be returned.
+     *
+     * @return const char* A pointer to error messages
+     */
     const char* what() const noexcept override
     {
         return m_error_msg.c_str();
     }
 
 private:
+    /** An error message holder. */
     std::string m_error_msg;
 };
 
