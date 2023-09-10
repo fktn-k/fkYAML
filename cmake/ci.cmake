3cf7e1a508223dc47fbd7ab4edea0f25f7046cb5
fktn <fktn.dev@gmail.com>
2023-09-11 02:17:12 +0900

    fixed to use explicitly clang++ compilers for jobs on self-hosted runner

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -1,6 +1,6 @@
 # find necessary tools
 
-foreach(COMPILER g++-9 g++-10 g++-11 g++-12 clang-11 clang-12 clang-13 clang-14 clang-15)
+foreach(COMPILER g++-9 g++-10 g++-11 g++-12 clang++-11 clang++-12 clang++-13 clang++-14 clang++-15)
   find_program(COMPILER_TOOL NAMES ${COMPILER})
   if(COMPILER_TOOL)
     add_custom_target(ci_test_compiler_${COMPILER}
