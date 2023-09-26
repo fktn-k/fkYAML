9b0a79fa8b6d92cb7d853336994b2c7c70b53b09
fktn <fktn.dev@gmail.com>
2023-09-26 22:10:48 +0900

    fixed minimum docker image tag for gcc

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -5,11 +5,6 @@ execute_process(COMMAND ${CLANGXX_TOOL} --version OUTPUT_VARIABLE CLANGXX_TOOL_V
 string(REGEX MATCH "[0-9]+(\\.[0-9]+)+" CLANGXX_TOOL_VERSION "${CLANGXX_TOOL_VERSION}")
 message(STATUS "# clang++ # version: ${CLANGXX_TOOL_VERSION} path: ${CLANGXX_TOOL}")
 
-find_program(GXX_TOOL NAMES g++-12 g++-11 g++-10 g++-9 g++ REQUIRED)
-execute_process(COMMAND ${GXX_TOOL} --version OUTPUT_VARIABLE GXX_TOOL_VERSION ERROR_VARIABLE GXX_TOOL_VERSION)
-string(REGEX MATCH "[0-9]+(\\.[0-9]+)+" GXX_TOOL_VERSION "${GXX_TOOL_VERSION}")
-message(STATUS "# g++ # version: ${GXX_TOOL_VERSION} path: ${GXX_TOOL}")
-
 find_program(IWYU_TOOL NAMES include-what-you-use iwyu REQUIRED)
 execute_process(COMMAND ${IWYU_TOOL} --version OUTPUT_VARIABLE IWYU_TOOL_VERSION ERROR_VARIABLE IWYU_TOOL_VERSION)
 string(REGEX MATCH "[0-9]+(\\.[0-9]+)+" IWYU_TOOL_VERSION "${IWYU_TOOL_VERSION}")
@@ -20,11 +15,11 @@ execute_process(COMMAND ${NINJA_TOOL} --version OUTPUT_VARIABLE NINJA_TOOL_VERSI
 string(REGEX MATCH "[0-9]+(\\.[0-9]+)+" NINJA_TOOL_VERSION "${NINJA_TOOL_VERSION}")
 message(STATUS "# Ninja # version: ${NINJA_TOOL_VERSION} path: ${NINJA_TOOL}")
 
-#############################################################
-#  Execute unit tests with various versions of clang++/g++  #
-#############################################################
+#########################################################
+#  Execute unit tests with various versions of clang++  #
+#########################################################
 
-foreach(COMPILER clang++-11 clang++-12 clang++-13 clang++-14 clang++-15 g++-9 g++-10 g++-11 g++-12)
+foreach(COMPILER clang++-11 clang++-12 clang++-13 clang++-14 clang++-15)
   find_program(COMPILER_TOOL NAMES ${COMPILER})
   if(COMPILER_TOOL)
     add_custom_target(ci_test_compiler_${COMPILER}
@@ -38,9 +33,9 @@ foreach(COMPILER clang++-11 clang++-12 clang++-13 clang++-14 clang++-15 g++-9 g+
   unset(COMPILER_TOOL CACHE)
 endforeach()
 
-###################################################################
-#  Execute unit tests with clang++/g++ for various c++ standards  #
-###################################################################
+###############################################################
+#  Execute unit tests with clang++ for various c++ standards  #
+###############################################################
 
 foreach(TARGET_CXX_STANDARD 11 14 17 20 23)
   add_custom_target(ci_test_clang++_c++${TARGET_CXX_STANDARD}
@@ -50,15 +45,6 @@ foreach(TARGET_CXX_STANDARD 11 14 17 20 23)
     COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/build_clang++_c++${TARGET_CXX_STANDARD} --config Debug
     COMMAND cd ${PROJECT_BINARY_DIR}/build_clang++_c++${TARGET_CXX_STANDARD} && ${CMAKE_CTEST_COMMAND} -C Debug --output-on-failure
     COMMENT "Compile and test with clang++ for C++${TARGET_CXX_STANDARD}"
-  )
-
-  add_custom_target(ci_test_g++_c++${TARGET_CXX_STANDARD}
-    COMMAND CXX=${GXX_TOOL} ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=Debug -GNinja -DFK_YAML_BUILD_ALL_TEST=ON
-      -DFK_YAML_TEST_TARGET_CXX_STANDARD=${TARGET_CXX_STANDARD}
-      -S${PROJECT_SOURCE_DIR} -B${PROJECT_BINARY_DIR}/build_g++_c++${TARGET_CXX_STANDARD}
-    COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/build_g++_c++${TARGET_CXX_STANDARD} --config Debug
-    COMMAND cd ${PROJECT_BINARY_DIR}/build_g++_c++${TARGET_CXX_STANDARD} && ${CMAKE_CTEST_COMMAND} -C Debug --output-on-failure
-    COMMENT "Compile and test with g++ for C++${CXX_STANDARD}"
   )
 endforeach()
 
