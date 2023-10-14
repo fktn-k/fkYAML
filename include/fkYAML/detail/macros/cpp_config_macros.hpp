7962fa7654f062b9938eb696ec8f95f0074289cc
fktn <fktn.dev@gmail.com>
2023-10-14 10:18:56 +0900

    extracted c++ standard/feature related macros to cpp_config_macros.hpp

---
+++ include/fkYAML/detail/macros/cpp_config_macros.hpp
@@ -0,0 +1,45 @@
+/**
+ *  _______   __ __   __  _____   __  __  __
+ * |   __| |_/  |  \_/  |/  _  \ /  \/  \|  |     fkYAML: A C++ header-only YAML library
+ * |   __|  _  < \_   _/|  ___  |    _   |  |___  version 0.0.1
+ * |__|  |_| \__|  |_|  |_|   |_|___||___|______| https://github.com/fktn-k/fkYAML
+ *
+ * SPDX-FileCopyrightText: 2023 Kensuke Fukutani <fktn.dev@gmail.com>
+ * SPDX-License-Identifier: MIT
+ *
+ * @file
+ */
+
+#ifndef FK_YAML_DETAIL_MACROS_CPP_CONFIG_MACROS_HPP_
+#define FK_YAML_DETAIL_MACROS_CPP_CONFIG_MACROS_HPP_
+
+// This file is assumed to be included only by version_macros.hpp file.
+// To avoid redundant inclusion, do not include version_macros.hpp file as the other files do.
+
+// C++ language standard detection (__cplusplus is not yet defined for C++23)
+// Skip detection if the definitions listed below already exist.
+#if !defined(FK_YAML_HAS_CXX_20) && !defined(FK_YAML_HAS_CXX_17) && !defined(FK_YAML_HAS_CXX_14) &&                    \
+    !defined(FK_YAML_CXX_11)
+    #if (defined(__cplusplus) && __cplusplus >= 202002L) || (defined(_MSVC_LANG) && MSVC_LANG >= 202002L)
+        #define FK_YAML_HAS_CXX_20
+        #define FK_YAML_HAS_CXX_17
+        #define FK_YAML_HAS_CXX_14
+    #elif (defined(__cplusplus) && __cplusplus >= 201703L) || (defined(_HAS_CXX17) && _HAS_CXX17 == 1)
+        #define FK_YAML_HAS_CXX_17
+        #define FK_YAML_HAS_CXX_14
+    #elif (defined(__cplusplus) && __cplusplus >= 201402L) || (defined(_HAS_CXX14) && _HAS_CXX14 == 1)
+        #define FK_YAML_HAS_CXX_14
+    #endif
+
+    // C++11 is the minimum required version of the fkYAML library.
+    #define FK_YAML_HAS_CXX_11
+#endif
+
+// switch usage of inline variables. Inline variables have been introduced since C++17.
+#if defined(FK_YAML_HAS_CXX_17)
+    #define FK_YAML_INLINE_VAR inline
+#else
+    #define FK_YAML_INLINE_VAR
+#endif
+
+#endif /* FK_YAML_DETAIL_MACROS_CPP_CONFIG_MACROS_HPP_ */
--- include/fkYAML/detail/macros/version_macros.hpp
+++ include/fkYAML/detail/macros/version_macros.hpp
@@ -10,55 +10,40 @@
  * @file
  */
 
+// Check version definitions if already defined.
 #if defined(FK_YAML_MAJOR_VERSION) && defined(FK_YAML_MINOR_VERSION) && defined(FK_YAML_PATCH_VERSION)
     #if FK_YAML_MAJOR_VERSION != 0 || FK_YAML_MINOR_VERSION != 0 || FK_YAML_PATCH_VERSION != 1
-        #warning "Already included a different version of fkYAML library!"
+        #warning Already included a different version of the fkYAML library!
+    #else
+        // define macros to skip defining macros down below.
+        #define FK_YAML_VERCHECK_SUCCEEDED
     #endif
 #endif
 
-#define FK_YAML_MAJOR_VERSION 0
-#define FK_YAML_MINOR_VERSION 0
-#define FK_YAML_PATCH_VERSION 1
+#ifndef FK_YAML_VERCHECK_SUCCEEDED
 
-#define FK_YAML_NAMESPACE_VERSION_CONCAT_EX(major, minor, patch) v##major##_##minor##_##patch
+    #define FK_YAML_MAJOR_VERSION 0
+    #define FK_YAML_MINOR_VERSION 0
+    #define FK_YAML_PATCH_VERSION 1
 
-#define FK_YAML_NAMESPACE_VERSION_CONCAT(major, minor, patch) FK_YAML_NAMESPACE_VERSION_CONCAT_EX(major, minor, patch)
+    #define FK_YAML_NAMESPACE_VERSION_CONCAT_IMPL(major, minor, patch) v##major##_##minor##_##patch
 
-#define FK_YAML_NAMESPACE_VERSION                                                                                      \
-    FK_YAML_NAMESPACE_VERSION_CONCAT(FK_YAML_MAJOR_VERSION, FK_YAML_MINOR_VERSION, FK_YAML_PATCH_VERSION)
+    #define FK_YAML_NAMESPACE_VERSION_CONCAT(major, minor, patch)                                                      \
+        FK_YAML_NAMESPACE_VERSION_CONCAT_IMPL(major, minor, patch)
 
-#define FK_YAML_NAMESPACE_BEGIN                                                                                        \
-    namespace fkyaml                                                                                                   \
-    {                                                                                                                  \
-    inline namespace FK_YAML_NAMESPACE_VERSION                                                                         \
-    {
+    #define FK_YAML_NAMESPACE_VERSION                                                                                  \
+        FK_YAML_NAMESPACE_VERSION_CONCAT(FK_YAML_MAJOR_VERSION, FK_YAML_MINOR_VERSION, FK_YAML_PATCH_VERSION)
 
-#define FK_YAML_NAMESPACE_END                                                                                          \
-    } /* namespace (inline namespace) */                                                                               \
-    } // namespace fkyaml
+    #define FK_YAML_NAMESPACE_BEGIN                                                                                    \
+        namespace fkyaml                                                                                               \
+        {                                                                                                              \
+        inline namespace FK_YAML_NAMESPACE_VERSION                                                                     \
+        {
 
-// C++ language standard detection (__cplusplus is not yet defined for C++23)
-// Skip detection if the definitions listed below already exist.
-#if !defined(FK_YAML_HAS_CXX_20) && !defined(FK_YAML_HAS_CXX_17) && !defined(FK_YAML_HAS_CXX_14) &&                    \
-    !defined(FK_YAML_CXX_11)
-    #if (defined(__cplusplus) && __cplusplus >= 202002L) || (defined(_MSVC_LANG) && MSVC_LANG >= 202002L)
-        #define FK_YAML_HAS_CXX_20
-        #define FK_YAML_HAS_CXX_17
-        #define FK_YAML_HAS_CXX_14
-    #elif (defined(__cplusplus) && __cplusplus >= 201703L) || (defined(_HAS_CXX17) && _HAS_CXX17 == 1)
-        #define FK_YAML_HAS_CXX_17
-        #define FK_YAML_HAS_CXX_14
-    #elif (defined(__cplusplus) && __cplusplus >= 201402L) || (defined(_HAS_CXX14) && _HAS_CXX14 == 1)
-        #define FK_YAML_HAS_CXX_14
-    #endif
+    #define FK_YAML_NAMESPACE_END                                                                                      \
+        } /* namespace (inline namespace) */                                                                           \
+        } // namespace fkyaml
 
-    // C++11 is the minimum required version of the fkYAML library.
-    #define FK_YAML_HAS_CXX_11
-#endif
+    #include "fkYAML/detail/macros/cpp_config_macros.hpp"
 
-// switch usage of inline variables. Inline variables are available since C++17.
-#if defined(FK_YAML_HAS_CXX_17)
-    #define FK_YAML_INLINE_VAR inline
-#else
-    #define FK_YAML_INLINE_VAR
-#endif
+#endif // !defined(FK_YAML_VERCHECK_SUCCEEDED)
