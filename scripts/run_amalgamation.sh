ea835ff0ee4a7dba2e427c0dbe7de29bd2bf79e5
fktn <fktn.dev@gmail.com>
2024-04-27 22:05:20 +0900

    fixed potential globbing and word splitting reported by Codacy

--- scripts/run_amalgamation.sh
+++ scripts/run_amalgamation.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-CALLER_DIR=$(pwd)
-cd "$(dirname "$0")"/..
-
-python3 ./tool/amalgamation/amalgamate.py -c ./tool/amalgamation/fkYAML.json -s . --verbose=yes
-
-cd $CALLER_DIR
+ROOT_DIR="$(dirname "$0")"/..
+python3 "$ROOT_DIR"/tool/amalgamation/amalgamate.py -c "$ROOT_DIR"/tool/amalgamation/fkYAML.json -s . --verbose=yes
--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-CALLER_DIR=$(pwd)
 SCRIPT_DIR="$(dirname "$0")"
-ROOT_DIR=$(cd $SCRIPT_DIR/.. && pwd)
-
-cd $SCRIPT_DIR
+ROOT_DIR="$SCRIPT_DIR/.."
 
 # install the clang-format package if not installed yet.
-if [ ! -e venv_clang_format/bin/clang-format ]; then
-    python3 -m venv venv_clang_format
-    venv_clang_format/bin/pip install clang-format==14.0.0
+if [ ! -e "$SCRIPT_DIR"/venv_clang_format/bin/clang-format ]; then
+    python3 -m venv "$SCRIPT_DIR/venv_clang_format"
+    "$SCRIPT_DIR"/venv_clang_format/bin/pip install clang-format==14.0.0
 fi
 
 for f in $(find "$ROOT_DIR/include" "$ROOT_DIR/test" "$ROOT_DIR/docs/examples" -type f -name "*.hpp" -o -name "*.cpp" | sort); do
-    venv_clang_format/bin/clang-format "$f" -i
+    "$SCRIPT_DIR"/venv_clang_format/bin/clang-format "$f" -i
 done
-
-cd $CALLER_DIR
