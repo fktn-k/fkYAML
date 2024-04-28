6eeed6d27a457c95cb73bdce4a52f12a4a0bd978
fktn <fktn.dev@gmail.com>
2024-04-28 14:22:00 +0900

    Fixed no such file/directory error when running Bash scripts on Windows (#328)
    * Fixed no such file/directory error when running Bash scripts on Windows
    
    * fixed missing $ to get the value of a Bash variable

--- scripts/run_amalgamation.sh
+++ scripts/run_amalgamation.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+set -eu
+
+# Platform switches are taken from https://gist.github.com/prabirshrestha/3080525
+UNAME=$(uname)
+if [ "$UNAME" = "Linux" ] || [ "$UNAME" = "Darwin" ]; then
+    PYTHON_EXE=python3
+else
+    # Use the Python launcher if the runtime environment is Windows.
+    # No version option (`-<version>`) since running with the latest version should be enough.
+    PYTHON_EXE=py
+fi
+
 ROOT_DIR="$(dirname "$0")"/..
-python3 "$ROOT_DIR"/tool/amalgamation/amalgamate.py -c "$ROOT_DIR"/tool/amalgamation/fkYAML.json -s . --verbose=yes
+AMALGAMATION_TOOL_DIR="$ROOT_DIR/tool/amalgamation"
+"$PYTHON_EXE" "$AMALGAMATION_TOOL_DIR"/amalgamate.py -c "$AMALGAMATION_TOOL_DIR"/fkYAML.json -s . --verbose=yes
