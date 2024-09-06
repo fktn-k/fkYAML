80eb6a4532abc4c0dc06066d70dbee6f2915cb73
fktn <fktn.dev@gmail.com>
2024-09-07 03:01:58 +0900

    added bounds check into atoi()

--- include/fkYAML/detail/conversions/scalar_conv.hpp
+++ include/fkYAML/detail/conversions/scalar_conv.hpp
@@ -26,6 +26,188 @@
 
 FK_YAML_DETAIL_NAMESPACE_BEGIN
 
+template <std::size_t NumBytes>
+struct conv_limits_base {};
+
+template <>
+struct conv_limits_base<1> {
+    /// max characters for octals (0o377) without the prefix part.
+    static constexpr std::size_t max_chars_oct = 3;
+    /// max characters for hexadecimals (0xFF) without the prefix part.
+    static constexpr std::size_t max_chars_hex = 2;
+
+    static bool check_if_octs_safe(const char* octs, std::size_t len) noexcept {
+        return (len < max_chars_oct) || (len == max_chars_oct && octs[0] <= '3');
+    }
+
+    static bool check_if_hexs_safe(const char* /*unused*/, std::size_t len) noexcept {
+        return len <= max_chars_hex;
+    }
+};
+
+template <>
+struct conv_limits_base<2> {
+    /// max characters for octals (0o177777) without the prefix part.
+    static constexpr std::size_t max_chars_oct = 6;
+    /// max characters for hexadecimals (0xFFFF) without the prefix part.
+    static constexpr std::size_t max_chars_hex = 4;
+
+    static bool check_if_octs_safe(const char* octs, std::size_t len) noexcept {
+        return (len < max_chars_oct) || (len == max_chars_oct && octs[0] <= '1');
+    }
+
+    static bool check_if_hexs_safe(const char* /*unused*/, std::size_t len) noexcept {
+        return len <= max_chars_hex;
+    }
+};
+
+template <>
+struct conv_limits_base<4> {
+    /// max characters for octals (0o37777777777) without the prefix part.
+    static constexpr std::size_t max_chars_oct = 11;
+    /// max characters for hexadecimals (0xFFFFFFFF) without the prefix part.
+    static constexpr std::size_t max_chars_hex = 8;
+
+    static bool check_if_octs_safe(const char* octs, std::size_t len) noexcept {
+        return (len < max_chars_oct) || (len == max_chars_oct && octs[0] <= '3');
+    }
+
+    static bool check_if_hexs_safe(const char* /*unused*/, std::size_t len) noexcept {
+        return len <= max_chars_hex;
+    }
+};
+
+template <>
+struct conv_limits_base<8> {
+    /// max characters for octals (0o1777777777777777777777) without the prefix part.
+    static constexpr std::size_t max_chars_oct = 22;
+    /// max characters for hexadecimals (0xFFFFFFFFFFFFFFFF) without the prefix part.
+    static constexpr std::size_t max_chars_hex = 16;
+
+    static bool check_if_octs_safe(const char* octs, std::size_t len) noexcept {
+        return (len < max_chars_oct) || (len == max_chars_oct && octs[0] <= '1');
+    }
+
+    static bool check_if_hexs_safe(const char* /*unused*/, std::size_t len) noexcept {
+        return len <= max_chars_hex;
+    }
+};
+
+template <std::size_t NumBytes, bool IsSigned>
+struct conv_limits {};
+
+template <>
+struct conv_limits<1, true> : conv_limits_base<1> {
+    /// with or without sign.
+    static constexpr bool is_signed = true;
+
+    /// max characters for decimals (-128..127) without sign.
+    static constexpr std::size_t max_chars_dec = 3;
+
+    /// string representation of max decimal value.
+    static constexpr char max_value_chars_dec[] = "127";
+    /// string representation of min decimal value without sign.
+    static constexpr char min_value_chars_dec[] = "128";
+};
+
+template <>
+struct conv_limits<1, false> : conv_limits_base<1> {
+    /// with or without sign.
+    static constexpr bool is_signed = false;
+
+    /// max characters for decimals (0..255) without sign.
+    static constexpr std::size_t max_chars_dec = 3;
+
+    /// string representation of max decimal value.
+    static constexpr char max_value_chars_dec[] = "255";
+    /// string representation of min decimal value.
+    static constexpr char min_value_chars_dec[] = "0";
+};
+
+template <>
+struct conv_limits<2, true> : conv_limits_base<2> {
+    /// with or without sign.
+    static constexpr bool is_signed = true;
+
+    /// max characters for decimals (-32768..32767) without sign.
+    static constexpr std::size_t max_chars_dec = 5;
+
+    /// string representation of max decimal value.
+    static constexpr char max_value_chars_dec[] = "32767";
+    /// string representation of min decimal value without sign.
+    static constexpr char min_value_chars_dec[] = "32768";
+};
+
+template <>
+struct conv_limits<2, false> : conv_limits_base<2> {
+    /// with or without sign.
+    static constexpr bool is_signed = false;
+
+    /// max characters for decimals (0..65535) without sign.
+    static constexpr std::size_t max_chars_dec = 5;
+
+    /// string representation of max decimal value.
+    static constexpr char max_value_chars_dec[] = "65535";
+    /// string representation of min decimal value.
+    static constexpr char min_value_chars_dec[] = "0";
+};
+
+template <>
+struct conv_limits<4, true> : conv_limits_base<4> {
+    /// with or without sign.
+    static constexpr bool is_signed = true;
+
+    /// max characters for decimals (-2147483648..2147483647) without sign.
+    static constexpr std::size_t max_chars_dec = 10;
+
+    /// string representation of max decimal value.
+    static constexpr char max_value_chars_dec[] = "2147483647";
+    /// string representation of min decimal value without sign.
+    static constexpr char min_value_chars_dec[] = "2147483648";
+};
+
+template <>
+struct conv_limits<4, false> : conv_limits_base<4> {
+    /// with or without sign.
+    static constexpr bool is_signed = false;
+
+    /// max characters for decimals (0..4294967295) without sign.
+    static constexpr std::size_t max_chars_dec = 10;
+
+    /// string representation of max decimal value.
+    static constexpr char max_value_chars_dec[] = "4294967295";
+    /// string representation of min decimal value.
+    static constexpr char min_value_chars_dec[] = "0";
+};
+
+template <>
+struct conv_limits<8, true> : conv_limits_base<8> {
+    /// with or without sign.
+    static constexpr bool is_signed = true;
+
+    /// max characters for decimals (-9223372036854775808..9223372036854775807) without sign.
+    static constexpr std::size_t max_chars_dec = 19;
+
+    /// string representation of max decimal value.
+    static constexpr char max_value_chars_dec[] = "9223372036854775807";
+    /// string representation of min decimal value without sign.
+    static constexpr char min_value_chars_dec[] = "9223372036854775808";
+};
+
+template <>
+struct conv_limits<8, false> : conv_limits_base<8> {
+    /// with or without sign.
+    static constexpr bool is_signed = false;
+
+    /// max characters for decimals (0..18446744073709551615) without sign.
+    static constexpr std::size_t max_chars_dec = 20;
+
+    /// string representation of max decimal value.
+    static constexpr char max_value_chars_dec[] = "18446744073709551615";
+    /// string representation of min decimal value.
+    static constexpr char min_value_chars_dec[] = "0";
+};
+
 template <typename CharItr>
 inline bool aton(CharItr begin, CharItr end, std::nullptr_t& /*unused*/) noexcept {
     static_assert(is_iterator_of<CharItr, char>::value, "atoi_dec() accepts iterators for char type");
@@ -84,63 +266,138 @@ inline bool atob(CharItr begin, CharItr end, BoolType& boolean) noexcept {
     return false;
 }
 
-template <typename CharItr, typename IntType>
-inline bool atoi_dec(CharItr begin, CharItr end, IntType& i) noexcept {
-    static_assert(is_iterator_of<CharItr, char>::value, "atoi_dec() accepts iterators for char type");
+template <typename IntType>
+inline bool atoi_dec_common(const char* p_begin, const char* p_end, IntType& i) noexcept {
     static_assert(
         is_non_bool_integral<IntType>::value, "atoi_dec() accepts non-boolean integral types as an output type");
 
-    if (begin == end) {
-        return false;
-    }
-
     i = 0;
     do {
-        char c = *begin;
+        char c = *p_begin;
         if (c < '0' || '9' < c) {
             return false;
         }
         i = i * IntType(10) + IntType(c - '0');
-    } while (++begin != end);
+    } while (++p_begin != p_end);
 
     return true;
 }
 
-template <typename CharItr, typename IntType>
-inline bool atoi_oct(CharItr begin, CharItr end, IntType& i) noexcept {
-    static_assert(is_iterator_of<CharItr, char>::value, "atoi_oct() accepts iterators for char type");
+template <typename IntType>
+inline bool atoi_dec_pos(const char* p_begin, const char* p_end, IntType& i) noexcept {
+    static_assert(
+        is_non_bool_integral<IntType>::value, "atoi_dec() accepts non-boolean integral types as an output type");
+
+    if (p_begin == p_end) {
+        return false;
+    }
+
+    using conv_limits_type = conv_limits<sizeof(IntType), std::is_signed<IntType>::value>;
+
+    std::size_t len = static_cast<std::size_t>(p_end - p_begin);
+    if (len > conv_limits_type::max_chars_dec) {
+        // Overflow will happen.
+        return false;
+    }
+
+    if (len == conv_limits_type::max_chars_dec) {
+        for (std::size_t i = 0; i < conv_limits_type::max_chars_dec; i++) {
+            if (p_begin[i] < conv_limits_type::max_value_chars_dec[i]) {
+                // No need to check the lower digits. Overflow will no longer happen.
+                break;
+            }
+
+            if (p_begin[i] > conv_limits_type::max_value_chars_dec[i]) {
+                // Overflow will happen.
+                return false;
+            }
+        }
+    }
+
+    return atoi_dec_common(p_begin, p_end, i);
+}
+
+template <typename IntType>
+inline bool atoi_dec_neg(const char* p_begin, const char* p_end, IntType& i) noexcept {
+    static_assert(
+        is_non_bool_integral<IntType>::value, "atoi_dec() accepts non-boolean integral types as an output type");
+
+    if (p_begin == p_end) {
+        return false;
+    }
+
+    using conv_limits_type = conv_limits<sizeof(IntType), std::is_signed<IntType>::value>;
+
+    std::size_t len = static_cast<std::size_t>(p_end - p_begin);
+    if (len > conv_limits_type::max_chars_dec) {
+        // Underflow will happen.
+        return false;
+    }
+
+    if (len == conv_limits_type::max_chars_dec) {
+        for (std::size_t i = 0; i < conv_limits_type::max_chars_dec; i++) {
+            if (p_begin[i] < conv_limits_type::min_value_chars_dec[i]) {
+                // No need to check the lower digits. Underflow will no longer happen.
+                break;
+            }
+
+            if (p_begin[i] > conv_limits_type::min_value_chars_dec[i]) {
+                // Underflow will happen.
+                return false;
+            }
+        }
+    }
+
+    return atoi_dec_common(p_begin, p_end, i);
+}
+
+template <typename IntType>
+inline bool atoi_oct(const char* p_begin, const char* p_end, IntType& i) noexcept {
     static_assert(
         is_non_bool_integral<IntType>::value, "atoi_oct() accepts non-boolean integral types as an output type");
 
-    if (begin == end) {
+    if (p_begin == p_end) {
+        return false;
+    }
+
+    using conv_limits_type = conv_limits<sizeof(IntType), std::is_signed<IntType>::value>;
+
+    std::size_t len = static_cast<std::size_t>(p_end - p_begin);
+    if (!conv_limits_type::check_if_octs_safe(p_begin, len)) {
         return false;
     }
 
     i = 0;
     do {
-        char c = *begin;
+        char c = *p_begin;
         if (c < '0' || '7' < c) {
             return false;
         }
         i = i * IntType(8) + IntType(c - '0');
-    } while (++begin != end);
+    } while (++p_begin != p_end);
 
     return true;
 }
 
-template <typename CharItr, typename IntType>
-inline bool atoi_hex(CharItr begin, CharItr end, IntType& i) noexcept {
-    static_assert(is_iterator_of<CharItr, char>::value, "atoi_hex() accepts iterators for char type");
+template <typename IntType>
+inline bool atoi_hex(const char* p_begin, const char* p_end, IntType& i) noexcept {
     static_assert(
         is_non_bool_integral<IntType>::value, "atoi_hex() accepts non-boolean integral types as an output type");
 
-    if (begin == end) {
+    if (p_begin == p_end) {
+        return false;
+    }
+
+    using conv_limits_type = conv_limits<sizeof(IntType), std::is_signed<IntType>::value>;
+
+    std::size_t len = static_cast<std::size_t>(p_end - p_begin);
+    if (!conv_limits_type::check_if_hexs_safe(p_begin, len)) {
         return false;
     }
 
     i = 0;
     do {
-        char c = *begin;
+        char c = *p_begin;
         IntType ci = 0;
         if ('0' <= c && c <= '9') {
             ci = IntType(c - '0');
@@ -155,7 +412,7 @@ inline bool atoi_hex(CharItr begin, CharItr end, IntType& i) noexcept {
             return false;
         }
         i = i * IntType(16) + ci;
-    } while (++begin != end);
+    } while (++p_begin != p_end);
 
     return true;
 }
@@ -169,9 +426,13 @@ inline bool atoi(CharItr begin, CharItr end, IntType& i) noexcept {
         return false;
     }
 
+    uint32_t len = static_cast<uint32_t>(std::distance(begin, end));
+    const char* p_begin = &*begin;
+    const char* p_end = p_begin + len;
+
     char first = *begin;
     if (first == '+') {
-        return atoi_dec(begin + 1, end, i);
+        return atoi_dec_pos(p_begin + 1, p_end, i);
     }
 
     if (first == '-') {
@@ -179,7 +440,7 @@ inline bool atoi(CharItr begin, CharItr end, IntType& i) noexcept {
             return false;
         }
 
-        bool success = atoi_dec(begin + 1, end, i);
+        bool success = atoi_dec_neg(p_begin + 1, p_end, i);
         if (success) {
             i *= IntType(-1);
         }
@@ -188,14 +449,14 @@ inline bool atoi(CharItr begin, CharItr end, IntType& i) noexcept {
     }
 
     if (first != '0') {
-        return atoi_dec(begin, end, i);
+        return atoi_dec_pos(p_begin, p_end, i);
     }
-    else if (begin + 1 != end) {
-        switch (*(begin + 1)) {
+    else if (p_begin + 1 != p_end) {
+        switch (*(p_begin + 1)) {
         case 'o':
-            return atoi_oct(begin + 2, end, i);
+            return atoi_oct(p_begin + 2, p_end, i);
         case 'x':
-            return atoi_hex(begin + 2, end, i);
+            return atoi_hex(p_begin + 2, p_end, i);
         default:
             // The YAML spec doesn't allow decimals starting with 0.
             return false;
