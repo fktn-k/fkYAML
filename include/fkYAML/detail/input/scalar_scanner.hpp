31d375699beb0a354f96edf0f5b1fb69ba3916ba
fktn <fktn.dev@gmail.com>
2024-10-20 14:00:48 +0900

    Fix wrong scalar value type detection from floating point value token (#414)
    * fixed false error on floating point value with the exponent
    
    * fixed wrong scalar value type detections from some other floating point number formats

--- include/fkYAML/detail/input/scalar_scanner.hpp
+++ include/fkYAML/detail/input/scalar_scanner.hpp
@@ -137,7 +137,10 @@ private:
         case '-':
             return (len > 1) ? scan_negative_number(++itr, --len) : node_type::STRING;
         case '+':
-            return (len > 1) ? scan_decimal_number(++itr, --len, false) : node_type::STRING;
+            return (len > 1) ? scan_decimal_number(++itr, --len) : node_type::STRING;
+        case '.':
+            // some integer(s) required after the decimal point as a floating point value.
+            return (len > 1) ? scan_after_decimal_point(++itr, --len) : node_type::STRING;
         case '0':
             return (len > 1) ? scan_after_zero_at_first(++itr, --len) : node_type::INTEGER;
         case '1':
@@ -149,7 +152,7 @@ private:
         case '7':
         case '8':
         case '9':
-            return (len > 1) ? scan_decimal_number(++itr, --len, false) : node_type::INTEGER;
+            return (len > 1) ? scan_decimal_number(++itr, --len) : node_type::INTEGER;
         default:
             return node_type::STRING;
         }
@@ -163,7 +166,12 @@ private:
         FK_YAML_ASSERT(len > 0);
 
         if (is_digit(*itr)) {
-            return (len > 1) ? scan_decimal_number(++itr, --len, false) : node_type::INTEGER;
+            return (len > 1) ? scan_decimal_number(++itr, --len) : node_type::INTEGER;
+        }
+
+        if (*itr == '.') {
+            // some integer(s) required after "-." as a floating point value.
+            return (len > 1) ? scan_after_decimal_point(++itr, --len) : node_type::STRING;
         }
 
         return node_type::STRING;
@@ -183,14 +191,13 @@ private:
         }
 
         switch (*itr) {
-        case '.': {
-            if (len == 1) {
-                // 0 is omitted after `0.`.
-                return node_type::FLOAT;
-            }
-            node_type ret = scan_after_decimal_point(++itr, --len, true);
-            return (ret == node_type::STRING) ? node_type::STRING : node_type::FLOAT;
-        }
+        case '.':
+            // 0 can be omitted after `0.`.
+            return (len > 1) ? scan_after_decimal_point(++itr, --len) : node_type::FLOAT;
+        case 'e':
+        case 'E':
+            // some integer(s) required after the exponent sign as a floating point value.
+            return (len > 1) ? scan_after_exponent(++itr, --len) : node_type::STRING;
         case 'o':
             return (len > 1) ? scan_octal_number(++itr, --len) : node_type::STRING;
         case 'x':
@@ -203,31 +210,23 @@ private:
     /// @brief Detects a scalar value type by scanning the contents part starting with a decimal.
     /// @param itr The iterator to the beginning decimal element of the scalar.
     /// @param len The length of the scalar left unscanned.
-    /// @param has_decimal_point Whether a decimal point has already been found in the previous part.
     /// @return A detected scalar value type.
-    static node_type scan_decimal_number(const char* itr, uint32_t len, bool has_decimal_point) noexcept {
+    static node_type scan_decimal_number(const char* itr, uint32_t len) noexcept {
         FK_YAML_ASSERT(len > 0);
 
         if (is_digit(*itr)) {
-            return (len > 1) ? scan_decimal_number(++itr, --len, has_decimal_point) : node_type::INTEGER;
+            return (len > 1) ? scan_decimal_number(++itr, --len) : node_type::INTEGER;
         }
 
         switch (*itr) {
         case '.': {
-            if (has_decimal_point) {
-                // the token has more than one period, e.g., a semantic version `1.2.3`.
-                return node_type::STRING;
-            }
-            if (len == 1) {
-                // 0 is omitted after the decimal point
-                return node_type::FLOAT;
-            }
-            node_type ret = scan_after_decimal_point(++itr, --len, true);
-            return (ret == node_type::STRING) ? node_type::STRING : node_type::FLOAT;
+            // 0 can be omitted after the decimal point
+            return (len > 1) ? scan_after_decimal_point(++itr, --len) : node_type::FLOAT;
         }
         case 'e':
         case 'E':
-            return (len > 1) ? scan_after_exponent(++itr, --len, has_decimal_point) : node_type::STRING;
+            // some integer(s) required after the exponent
+            return (len > 1) ? scan_after_exponent(++itr, --len) : node_type::STRING;
         default:
             return node_type::STRING;
         }
@@ -236,37 +235,55 @@ private:
     /// @brief Detects a scalar value type by scanning the contents right after a decimal point.
     /// @param itr The iterator to the past-the-decimal-point element of the scalar.
     /// @param len The length of the scalar left unscanned.
-    /// @param has_decimal_point Whether the decimal point has already been found in the previous part.
     /// @return A detected scalar value type.
-    static node_type scan_after_decimal_point(const char* itr, uint32_t len, bool has_decimal_point) noexcept {
+    static node_type scan_after_decimal_point(const char* itr, uint32_t len) noexcept {
         FK_YAML_ASSERT(len > 0);
 
-        if (is_digit(*itr)) {
-            return (len > 1) ? scan_decimal_number(++itr, --len, has_decimal_point) : node_type::FLOAT;
+        for (uint32_t i = 0; i < len; i++) {
+            char c = *itr++;
+
+            if (is_digit(c)) {
+                continue;
+            }
+
+            if (c == 'e' || c == 'E') {
+                if (i == len - 1) {
+                    // some integer(s) required after the exponent
+                    return node_type::STRING;
+                }
+                return scan_after_exponent(itr, len - i - 1);
+            }
+
+            return node_type::STRING;
         }
 
-        return node_type::STRING;
+        return node_type::FLOAT;
     }
 
     /// @brief Detects a scalar value type by scanning the contents right after the exponent prefix ("e" or "E").
     /// @param itr The iterator to the past-the-exponent-prefix element of the scalar.
     /// @param len The length of the scalar left unscanned.
-    /// @param has_decimal_point Whether the decimal point has already been found in the previous part.
     /// @return A detected scalar value type.
-    static node_type scan_after_exponent(const char* itr, uint32_t len, bool has_decimal_point) noexcept {
+    static node_type scan_after_exponent(const char* itr, uint32_t len) noexcept {
         FK_YAML_ASSERT(len > 0);
 
-        if (is_digit(*itr)) {
-            return (len > 1) ? scan_decimal_number(++itr, --len, has_decimal_point) : node_type::FLOAT;
+        char c = *itr;
+        if (c == '+' || c == '-') {
+            if (len == 1) {
+                // some integer(s) required after the sign.
+                return node_type::STRING;
+            }
+            ++itr;
+            --len;
         }
 
-        switch (*itr) {
-        case '+':
-        case '-':
-            return (len > 1) ? scan_decimal_number(++itr, --len, has_decimal_point) : node_type::STRING;
-        default:
-            return node_type::STRING;
+        for (uint32_t i = 0; i < len; i++) {
+            if (!is_digit(*itr++)) {
+                return node_type::STRING;
+            }
         }
+
+        return node_type::FLOAT;
     }
 
     /// @brief Detects a scalar value type by scanning the contents assuming octal numbers.
