6cecd68366e6faca25b9f86677e386339587ba27
fktn <fktn.dev@gmail.com>
2024-11-12 20:39:55 +0900

    updated amalgamation scripts and CONTRIBUTING.md

--- scripts/check_amalgamation.sh
+++ scripts/check_amalgamation.sh
@@ -4,15 +4,27 @@ set -eu
 
 ROOT_DIR="$(dirname "$0")/.."
 
-SINGLE_HEADER_PATH="$ROOT_DIR/single_include/fkYAML/node.hpp"
-mv "$SINGLE_HEADER_PATH" "$SINGLE_HEADER_PATH~"
+NODE_HPP_PATH="$ROOT_DIR/single_include/fkYAML/node.hpp"
+FKYAML_FWD_HPP_PATH="$ROOT_DIR/single_include/fkYAML/fkyaml_fwd.hpp"
+
+mv "$NODE_HPP_PATH" "$NODE_HPP_PATH~"
+mv "$FKYAML_FWD_HPP_PATH" "$FKYAML_FWD_HPP_PATH~"
+
 "$ROOT_DIR"/scripts/run_amalgamation.sh >/dev/null
 
-if cmp -s "$SINGLE_HEADER_PATH" "$SINGLE_HEADER_PATH~" ; then
-    echo Amalgamation check passed!
+if cmp -s "$NODE_HPP_PATH" "$NODE_HPP_PATH~" ; then
+    echo Amalgamation check passed for node.hpp file!
 else
-    echo Amalgamation required.
+    echo Amalgamation required for node.hpp file.
     echo Please follow the guideline in the CONTRIBUTING.md file.
 fi
+mv "$NODE_HPP_PATH~" "$NODE_HPP_PATH"
 
-mv "$SINGLE_HEADER_PATH~" "$SINGLE_HEADER_PATH"
+if cmp -s "$FKYAML_FWD_HPP_PATH" "$FKYAML_FWD_HPP_PATH~" ; then
+    echo Amalgamation check passed for fkyaml_fwd.hpp file!
+else
+    echo Amalgamation required for fkyaml_fwd.hpp file.
+    echo Please follow the guideline in the CONTRIBUTING.md file.
+fi
+mv "$FKYAML_FWD_HPP_PATH~" "$FKYAML_FWD_HPP_PATH"
+
