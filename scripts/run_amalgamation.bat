d4fa8a89fe4b71168bd194c5850c9ac85fa20648
fktn <fktn.dev@gmail.com>
2025-01-02 23:35:29 +0900

    fixed paths in the script files

--- scripts/run_amalgamation.bat
+++ scripts/run_amalgamation.bat
@@ -4,11 +4,11 @@ setlocal
 set CALLER_DIR=%cd%
 
 set SCRIPT_PATH=%~dp0
-set AMALGAMATION_TOOL_DIR=%SCRIPT_PATH%\..\tool\amalgamation
+set AMALGAMATION_TOOLS_DIR=%SCRIPT_PATH%\..\tools\amalgamation
 cd %SCRIPT_PATH%\..
 
-python %AMALGAMATION_TOOL_DIR%\amalgamate.py -c %AMALGAMATION_TOOL_DIR%\node_hpp.json -s . --verbose=yes
-python %AMALGAMATION_TOOL_DIR%\amalgamate.py -c %AMALGAMATION_TOOL_DIR%\fkyaml_fwd_hpp.json -s . --verbose=yes
+python %AMALGAMATION_TOOLS_DIR%\amalgamate.py -c %AMALGAMATION_TOOLS_DIR%\node_hpp.json -s . --verbose=yes
+python %AMALGAMATION_TOOLS_DIR%\amalgamate.py -c %AMALGAMATION_TOOLS_DIR%\fkyaml_fwd_hpp.json -s . --verbose=yes
 
 if %errorlevel% == 0 (
     echo Amalgamation succeeded!
