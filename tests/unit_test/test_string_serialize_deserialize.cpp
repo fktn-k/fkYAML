e8a8942160ce4c8f1b13b86946f56732af7bf3c9
Sean Farrell <sean.farrell@rioki.org>
2025-11-17 22:30:57 +0100

    More coverage.

--- tests/unit_test/test_string_serialize_deserialize.cpp
+++ tests/unit_test/test_string_serialize_deserialize.cpp
@@ -6,14 +6,36 @@
 
 TEST_CASE("String_Serialize_Deserialize_Roundtrip") {
     fkyaml::node source = fkyaml::node::mapping();
-    source["pass"] = "***";
-    source["host"] = "localhost:7007";
+    source["alias"]      = "*anchor";
+    source["anchor"]     = "&anchor";
+    source["tag"]        = "!tag";
+    source["comment"]    = "#not-a-comment";
+    source["brackets"]   = "[1,2]";
+    source["braces"]     = "{a: 1}";
+    source["comma"]      = "a,b";
+    source["pipe"]       = "a|b";
+    source["gt"]         = "a>b";
+    source["at"]         = "user@host";
+    source["backtick"]   = "`code`";
+    source["percent"]    = "100%";
+    source["colon"]      = "key:value";
+    source["question"]   = "?maybe";
+    source["dash"]       = "-value";
+
+    // Values that should *not* trigger scane_yaml_tokens
+    source["plain"]      = "just_text";
+    source["empty"]      = "";
 
     std::stringstream buff;
     buff << source;
 
     fkyaml::node copy;
     REQUIRE_NOTHROW(copy = fkyaml::node::deserialize(buff));
-    REQUIRE(source["pass"] == copy["pass"]);
-    REQUIRE(source["host"] == copy["host"]);
+    for (const auto& kv : source.map_items()) {
+        const auto& key  = kv.key().as_str();
+        const auto& lhs  = source[key];
+        const auto& rhs  = copy[key];
+
+        REQUIRE(lhs == rhs);
+    }
 }
