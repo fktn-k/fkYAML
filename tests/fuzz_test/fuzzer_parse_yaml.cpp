345c082f9667f3bcb083b40c80f5a2f206c5563e
fktn-k <fktn.dev@gmail.com>
2026-08-08 12:54:55 +0900

    Add fuzz test for parsing YAML inputs

---
+++ tests/fuzz_test/fuzzer_parse_yaml.cpp
@@ -0,0 +1,21 @@
+//  _______   __ __   __  _____   __  __  __
+// |   __| |_/  |  \_/  |/  _  \ /  \/  \|  |     fkYAML: A C++ header-only YAML library (supporting code)
+// |   __|  _  < \_   _/|  ___  |    _   |  |___  version 0.4.3
+// |__|  |_| \__|  |_|  |_|   |_|___||___|______| https://github.com/fktn-k/fkYAML
+//
+// SPDX-FileCopyrightText: 2023-2026 Kensuke Fukutani <fktn.dev@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#include <fkYAML/node.hpp>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    try {
+        const char* data_str = reinterpret_cast<const char*>(data);
+        fkyaml::node n = fkyaml::node::deserialize(data_str, data_str + size);
+        (void)n;
+    }
+    catch (const fkyaml::exception& e) {
+        (void)e;
+    }
+    return 0;
+}
